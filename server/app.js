dfa9b08b0b143372b06dcfc5887e95190d6bd094
ajearle11 <ajearle9@gmail.com>
2023-06-12 14:57:21 +0100

    Added the api routes for the questions

--- server/app.js
+++ server/app.js
@@ -7,6 +7,20 @@ let questions = require("./questions.json");
 app.use(cors());
 app.use(express.json());
 
+//basically swaps the last item of the array each time with a random element from the list for array length revolutions
+const randomFunc = (array) => {
+  for (let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+};
+
+//function to filter through an object
+const filter = (filterWord) => {
+  return questions.filter((type) => type.category === filterWord);
+};
+
 //API homepage
 app.get("/", function (req, res) {
   res.send(
@@ -20,23 +34,31 @@ app.get("/questions", function (req, res) {
 });
 
 //10 ravenclaw questions
-app.get("/questions", function (req, res) {
-  res.send(questions);
+app.get("/ravenclaw", function (req, res) {
+  let filteredQuestions = filter("ravenclaw");
+  let result = randomFunc(filteredQuestions);
+  res.send(result);
 });
 
 //10 gryffindor questions
-app.get("/questions", function (req, res) {
-  res.send(questions);
+app.get("/gryffindor", function (req, res) {
+  let filteredQuestions = filter("gryffindor");
+  let result = randomFunc(filteredQuestions);
+  res.send(result);
 });
 
 //10 hufflepuff questions
-app.get("/questions", function (req, res) {
-  res.send(questions);
+app.get("/hufflepuff", function (req, res) {
+  let filteredQuestions = filter("Hufflepuff");
+  let result = randomFunc(filteredQuestions);
+  res.send(result);
 });
 
 //10 slytherin questions
-app.get("/questions", function (req, res) {
-  res.send(questions);
+app.get("/slytherin", function (req, res) {
+  let filteredQuestions = filter("slytherin");
+  let result = randomFunc(filteredQuestions);
+  res.send(result);
 });
 
 module.exports = app;
