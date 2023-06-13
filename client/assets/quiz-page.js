782e2a60b78d2f6ecdf2ccfc422012c2614384f4
ajearle11 <ajearle9@gmail.com>
2023-06-13 20:50:36 +0100

    Refactored the functions to make them smoother

--- client/assets/quiz-page.js
+++ client/assets/quiz-page.js
@@ -6,26 +6,29 @@ const answerThree = document.querySelector("#answer-three");
 const answerFour = document.querySelector("#answer-four");
 const tryAgain = document.querySelector(".incorrect-text");
 const score = document.querySelector("#points");
+const buttonList = document.querySelectorAll(".front button");
+const correctQuestion = document.querySelector("#correct-question");
+const correctAnswer = document.querySelector("#correct-answer");
+const extraInfo = document.querySelector("#extra-info");
+const nextQuestion = document.querySelector(".back button");
 
-const flipCard = () => {
-  card.classList.toggle("flipCard");
-};
-
-// card.addEventListener("click", flipCard);
+let i = 0;
+let runningScore = 0;
 
 let questionsArray = [];
 
-logJSONDataSlytherin = async () => {
+//GRAB THE CORRECT QUESTIONS
+const logJSONDataSlytherin = async () => {
   try {
-    const response = await fetch("http://localhost:3000/slytherin");
+    const response = await fetch("http://localhost:3000/Slytherin");
     if (response.ok) {
       const data = await response.json();
       questionsArray.push(data);
-      question.textContent = questionsArray[0][0].question;
-      answerOne.textContent = questionsArray[0][0].answers.first;
-      answerTwo.textContent = questionsArray[0][0].answers.second;
-      answerThree.textContent = questionsArray[0][0].answers.third;
-      answerFour.textContent = questionsArray[0][0].answers.fourth;
+      question.textContent = questionsArray[0][i].question;
+      answerOne.textContent = questionsArray[0][i].answers.first;
+      answerTwo.textContent = questionsArray[0][i].answers.second;
+      answerThree.textContent = questionsArray[0][i].answers.third;
+      answerFour.textContent = questionsArray[0][i].answers.fourth;
     } else {
       console.log("There is an error");
     }
@@ -34,57 +37,101 @@ logJSONDataSlytherin = async () => {
   }
 };
 
+//INITIAL CALL OF THE DATA
 logJSONDataSlytherin();
 
-const checkAnswerOne = () => {
-  if (questionsArray[0][0].correct == 0) {
-    answerOne.style.backgroundColor = "green";
-    answerOne.style.color = "white";
-    tryAgain.textContent = "That's correct- click me to find out more";
+//CHECKS THAT THE ANSWER GIVEN IS CORRECT
+const checkAnswer = (e) => {
+  if (e.target.id === "answer-one" && questionsArray[0][i].correct == 0) {
+    completesCheckAnswer(e, 0);
+  } else if (
+    e.target.id === "answer-two" &&
+    questionsArray[0][i].correct == 1
+  ) {
+    completesCheckAnswer(e, 1);
+  } else if (
+    e.target.id === "answer-three" &&
+    questionsArray[0][i].correct == 2
+  ) {
+    completesCheckAnswer(e, 2);
+  } else if (
+    e.target.id === "answer-four" &&
+    questionsArray[0][i].correct == 3
+  ) {
+    completesCheckAnswer(e, 3);
   } else {
     tryAgain.textContent = "That's incorrect- Try again";
-    answerOne.style.backgroundColor = "red";
-    answerOne.style.color = "white";
+    e.target.style.backgroundColor = "red";
+    e.target.style.color = "white";
   }
 };
 
-const checkAnswerTwo = () => {
-  if (questionsArray[0][0].correct == 1) {
-    answerTwo.style.backgroundColor = "green";
-    answerTwo.style.color = "white";
-    tryAgain.textContent = "That's correct- click me to find out more";
+//STYLES THE ANSWERS IF CORRECT
+const completesCheckAnswer = (e, number) => {
+  e.target.style.backgroundColor = "green";
+  e.target.style.color = "white";
+  backCardSetter(number);
+};
+
+//FLIPS THE CARD
+const flipCard = () => {
+  card.classList.toggle("flipCard");
+};
+
+//FUNCTION THAT FLIPS THE CARD AND THEN SETS THE TEXT
+const backCardSetter = (number) => {
+  let pointScore = 0;
+  card.addEventListener("click", flipCard);
+  setTimeout(() => card.removeEventListener("click", flipCard), 100);
+  correctAnswer.textContent = buttonList[number].innerHTML;
+  correctQuestion.textContent = questionsArray[0][i].question;
+  extraInfo.textContent = questionsArray[0][i].info;
+  buttonList.forEach((element) => {
+    if (element.style.backgroundColor == "red") {
+      pointScore += 1;
+    }
+  });
+  scoreSetter(pointScore);
+};
+
+//USED TO DETERMINE THE POINTS SCORED PER QUESTION
+const scoreSetter = (score) => {
+  if (score === 0) {
+    runningScore += 5;
+  } else if (score === 1) {
+    runningScore += 4;
+  } else if (score === 2) {
+    runningScore += 3;
   } else {
-    tryAgain.textContent = "That's incorrect- Try again";
-    answerTwo.style.backgroundColor = "red";
-    answerTwo.style.color = "white";
+    runningScore += 2;
   }
 };
 
-const checkAnswerThree = () => {
-  if (questionsArray[0][0].correct == 2) {
-    answerThree.style.backgroundColor = "green";
-    answerThree.style.color = "white";
-    tryAgain.textContent = "That's correct- click me to find out more";
+//RESETS ALL THE ELEMENTS AND REPLACES THE QUESTION WITH THE NEXT ONE
+const moveToNextQuestion = () => {
+  score.textContent = `Score: ${runningScore}`;
+  if (i < 9) {
+    i += 1;
+    for (let button of buttonList) {
+      button.style.backgroundColor = "#ccc";
+      button.style.color = "black";
+    }
+    question.textContent = questionsArray[0][i].question;
+    answerOne.textContent = questionsArray[0][i].answers.first;
+    answerTwo.textContent = questionsArray[0][i].answers.second;
+    answerThree.textContent = questionsArray[0][i].answers.third;
+    answerFour.textContent = questionsArray[0][i].answers.fourth;
+    card.classList.toggle("flipCard");
   } else {
-    tryAgain.textContent = "That's incorrect- Try again";
-    answerThree.style.backgroundColor = "red";
-    answerThree.style.color = "white";
+    //HERE IS WHERE THE FINAL PAGE WILL GO
+    console.log("Hi");
   }
 };
 
-const checkAnswerFour = () => {
-  if (questionsArray[0][0].correct == 3) {
-    answerFour.style.backgroundColor = "green";
-    answerFour.style.color = "white";
-    tryAgain.textContent = "That's correct- click me to find out more";
-  } else {
-    tryAgain.textContent = "That's incorrect- Try again";
-    answerFour.style.backgroundColor = "red";
-    answerFour.style.color = "white";
-  }
-};
+//BUTTON EVENT LISTENER FOR MOVING ON TO THE NEXT QUESTION
+nextQuestion.addEventListener("click", moveToNextQuestion);
 
-answerOne.addEventListener("click", checkAnswerOne);
-answerTwo.addEventListener("click", checkAnswerTwo);
-answerThree.addEventListener("click", checkAnswerThree);
-answerFour.addEventListener("click", checkAnswerFour);
+//GIVES AN EVENT LISTENER TO EACH ANSWER
+for (let button of buttonList) {
+  button.addEventListener("click", checkAnswer);
+}
